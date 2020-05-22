10f774d10c4b333701ec643eaadafb112cbae4a1
a <thierrybanel@free.fr>
2020-05-22 11:14:53 +0200

    time optimize result insertion in buffer

--- orgtbl-join.el
+++ orgtbl-join.el
@@ -229,25 +229,31 @@ special symbol 'hline to mean an horizontal line."
       ;; insert well padded and aligned cells at current buffer position
       (cl-loop for row in table
 	       do
-	       (if (listp row)
-		   (cl-loop for cell in row
-			    for mx in maxwidths
-			    for nu in numbers
-			    for pad = (- mx (length cell))
-			    do (cond ((<= pad 0)
-				      ;; no alignment
-				      (insert "| " cell " "))
-				     (nu
-				      ;; left alignment
-				      (insert "| " cell (make-string pad ? ) " "))
-				     (t
-				      ;; right alignment
-				      (insert "| " (make-string pad ? ) cell " "))))
-		 (cl-loop with bar = "|"
-			  for mx in maxwidths
-			  do (insert bar (make-string (+ mx 2) ?-))
-			  do (setq bar "+")))
-	       do (insert "|\n")))))
+	       (insert
+		(if (listp row)
+		    (cl-loop for cell in row
+			     for mx in maxwidths
+			     for nu in numbers
+			     for pad = (- mx (length cell))
+			     ;; time optimization: suprisingly,
+			     ;; (insert (concat a b c)) is faster than
+			     ;; (insert a b c)
+			     concat
+			     (cond ((<= pad 0)
+				    ;; no alignment
+				    (concat "| " cell " "))
+				   (nu
+				    ;; left alignment
+				    (concat "| " cell (make-string pad ? ) " "))
+				   (t
+				    ;; right alignment
+				    (concat "| " (make-string pad ? ) cell " "))))
+		  (cl-loop with bar = "|"
+			   for mx in maxwidths
+			   concat bar
+			   concat (make-string (+ mx 2) ?-)
+			   do (setq bar "+")))
+		"|\n")))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; In-place mode
