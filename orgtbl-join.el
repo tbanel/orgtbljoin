3e99ebc8ad013d846d9d987a7dce54dd6f83e52a
a <thierrybanel@free.fr>
2022-01-09 18:33:26 +0100

    update copyleft

--- orgtbl-join.el
+++ orgtbl-join.el
@@ -1,6 +1,6 @@
 ;;; orgtbl-join.el --- join columns from another table
 
-;; Copyright (C) 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021  Thierry Banel
+;; Copyright (C) 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022  Thierry Banel
 
 ;; Author: Thierry Banel tbanelwebmin at free dot fr
 ;; Contributors:
